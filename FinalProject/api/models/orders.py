aec6b257df67c1e1030d451e196ab32eaf55e3d2
Derek Wang <93870173+dmwang1234@users.noreply.github.com>
2026-04-15 15:18:51 -0400

    Rename payment to card_info in orders model, clarify the description column
    Updated the payment field to card_info and added a comment to the description field.

--- FinalProject/api/models/orders.py
+++ FinalProject/api/models/orders.py
@@ -10,7 +10,7 @@ class Order(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     customer_name = Column(String(100))
     order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
-    description = Column(String(300))
-    payment = Column(Integer, nullable=False)
+    description = Column(String(300)) #Will contain order items and total
+    card_info = Column(Integer, nullable=False)
 
     order_details = relationship("OrderDetail", back_populates="order")
