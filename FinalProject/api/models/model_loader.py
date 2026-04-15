0f5049f3d45b3d7e0811f25a9a20c469cfdaf29f
Derek Wang <93870173+dmwang1234@users.noreply.github.com>
2026-04-15 15:28:56 -0400

    Refactor model creation in index function
    Removed unnecessary model creation for orders, recipes, and sandwiches while adding promotions and reviews.

--- FinalProject/api/models/model_loader.py
+++ FinalProject/api/models/model_loader.py
@@ -5,7 +5,6 @@ from ..dependencies.database import engine
 
 def index():
     orders.Base.metadata.create_all(engine)
-    order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
+    promotions.Base.metadata.create_all(engine)
+    reviews.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
